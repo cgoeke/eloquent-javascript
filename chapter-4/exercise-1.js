b4e07c323d0b9026095ee7260df0119cab4c0f11
Christian Goeke <christianpgoeke@gmail.com>
2018-09-11 20:18:02 -0700

    The sum of a range

---
+++ chapter-4/exercise-1.js
@@ -0,0 +1,31 @@
+/*
+The sum of a range
+
+Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
+
+Next, write a sum function that takes an array of numbers and returns the sum of these numbers.
+
+As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+*/
+
+function range(start, end, step = start < end ? 1 : -1) {
+  let arr = [];
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) {
+      arr.push(i);
+    }
+  } else {
+    for (let i = start; i >= end; i += step) {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+
+function sum(arr) {
+  let total = 0;
+  for (let i of arr) {
+    total += i;
+  }
+  return total;
+}
